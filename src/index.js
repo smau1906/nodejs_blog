b57e68801ae3e799853bd29d9daef378ada4656e
Mau Luu <smau1906@gmail.com>
2020-12-27 11:22:36 +0700

    router and controller

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path')
-
+const route = require('./routes/index')
 const app = express();
 const port = 3000;
 
 //static file
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
 //HTTP logger
 app.use(morgan('combined'));
@@ -18,15 +22,10 @@ app.engine('hbs', handlebars({
     extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render("home")
-});
-
-app.get('/news', (req, res) => {
-    res.render("news")
-});
+// Route init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
